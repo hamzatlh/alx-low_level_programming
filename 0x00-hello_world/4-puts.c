7817fb28c78134dc29018c7991640569c46c651e
hamzatlh <htlhaouia@gmail.com>
2023-02-16 13:33:26 -0800

    vv

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - nn
+ * Description: 'prints'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

ec6dd412735d8fd08b928a931c32fdb571c47a98
hamzatlh <htlhaouia@gmail.com>
2023-02-17 13:15:48 -0800

    vb

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - something
+ * Description: 'complete the code'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; ++c)
+                putchar(c);
+	for (c = 'A'; c <= 'Z'; ++c)
+		putchar(c);
+        putchar('\n');
+        return (0);
+}

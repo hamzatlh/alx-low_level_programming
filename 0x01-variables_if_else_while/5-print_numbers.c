25edc90687d3cf5c84d3dea2ecbb1ee08a8677c8
hamzatlh <htlhaouia@gmail.com>
2023-02-17 13:29:22 -0800

    vb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - something
+ * Description: 'complete the code'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}

cf16e8ffb18e994d637414664fa603f2c2183ef2
hamzatlh <htlhaouia@gmail.com>
2023-02-21 12:41:25 -0800

    g

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - something
+ * Description: 'print alphabet'
+ * Return: Always 0 (Success)
+ */
+
+void	print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+}

971df81460fb5e00576e9c67c007e4d7214753b9
hamzatlh <htlhaouia@gmail.com>
2023-02-22 13:35:38 -0800

    ff

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,11 @@
  * Return: 0
  */
 
-int main(void)
+int main(int ac, )
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
-	return (0);
+return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 # define MAIN_H
 
-#include <unistd.h>
 #include <stdio.h>
 void positive_or_negative(int i);
 #endif

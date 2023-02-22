771158e4cca76e6651149b80492cf573931b2c7c
hamzatlh <htlhaouia@gmail.com>
2023-02-22 10:38:10 -0800

    rr

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,9 +24,9 @@ void print_to_98(int n)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
 				printf("%d\n", j);
 		}
 	}

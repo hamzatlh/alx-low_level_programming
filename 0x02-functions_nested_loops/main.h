4d87db1c291e4027c163ef77d9cf599221c0c1b7
hamzatlh <htlhaouia@gmail.com>
2023-02-23 10:01:19 -0800

    bb

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,15 @@
 void print_to_98(int n);
 int main(void);
 void    print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int add(int, int);
+int _abs(int);
+int print_last_digit(int);
+void times_table(void);
+void print_to_98(int n);
 int _putchar(char c);
 #endif
 

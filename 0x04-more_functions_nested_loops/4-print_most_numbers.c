e3e83eef80fda4672bfc4711eedaed27d42ecda1
hamzatlh <htlhaouia@gmaail.com>
2023-02-23 16:24:44 -0800

    hhB

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line.
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	write(1, "01356789\n", 9);
+}

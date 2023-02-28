5fecfe42bc6796323ddf74cc2f9ef8901194b234
hamzatlh <htlhaouia@gmail.com>
2023-02-28 15:22:16 -0800

    gg

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_array - print an array of ints
+ *@a: pointer to the first int of the array
+ *@n: the size of the array
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (i < n)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		i++;
+	}
+	printf("%d\n", a[n - 1]);
+}

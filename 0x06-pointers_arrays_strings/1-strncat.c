3252d99c82728c577ad80def988dfa7ec404be4a
hamzatlh <htlhaouia@gmail.com>
2023-03-02 15:13:08 -0800

    concatenate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strncat -a function that  concatenates tow strings
+ *@dest: destination
+ *@src: source
+ *@n: number of bytes to concatenate
+ *Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+		i++;
+	while (src[j] && j < n)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}

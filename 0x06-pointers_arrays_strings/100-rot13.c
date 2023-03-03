2ad7c9032dba6231cc5609b7af7e8d139f00f05f
hamzatlh <htlhaouia@gmail.com>
2023-03-02 16:05:18 -0800

    gg

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot_13 - encodes a string into rot13
+ * @s: string to encode
+ * Return: s
+ */
+char	*rot_13(char *s)
+{
+	int	idx;
+
+	idx = 0;
+	while (s[idx] != '\0')
+	{
+		if ((s[idx] >= 'A' && s[idx] <= 'M')
+			|| (s[idx] >= 'a' && s[idx] <= 'm'))
+			s[idx] += 13;
+		else if ((s[idx] >= 'N' && s[idx] <= 'Z')
+			|| (s[idx] >= 'n' && s[idx] <= 'z'))
+			s[idx] -= 13;
+		write(1, &s[i], 1);
+		idx++;
+	}
+	return (s);
+}

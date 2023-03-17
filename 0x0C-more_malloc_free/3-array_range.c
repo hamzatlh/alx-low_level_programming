3bcdf4b6922b0fa67a0b3a36acad63e3a02a33c0
hamzatlh <htlhaouia@gmail.com>
2023-03-16 17:36:00 -0700

    gg

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,11 +15,11 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min + 1;
-	array = malloc(sizeof(int) * size);
+	size = max - min;
+	array = malloc(sizeof(int) * size + 1);
 	if (!array)
 		return (NULL);
-	while (size)
+	while (min <= max)
 	{
 		array[i] = min;
 		min++;

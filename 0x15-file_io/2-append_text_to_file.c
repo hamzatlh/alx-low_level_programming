c99a541e469c180add421cdab4b6e6ae06426ab1
hamzatlh <talhaouiah@gmail.com>
2023-04-10 19:02:31 -0700

    fix it

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		len++;
 	write_it = write(fd, text_content, len);
 	if (write_it < 0)
+	{
+		close (fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
